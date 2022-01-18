5745179e9d843a78eeb54f3de180187f7706b03b
luacam20 <luana.cam20@gmail.com>
2022-01-18 18:27:00 -0300

    interface autenticaçao com stripe

---
+++ src/components/SiginButton/index.tsx
@@ -0,0 +1,28 @@
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+
+import styles from './styles.module.scss'
+
+export function SigInButton() {
+    
+    const isUserLoggedIn = true;
+
+    return isUserLoggedIn ? (
+        <button 
+            type="button" 
+            className={styles.signinButton}
+        >
+            <FaGithub color="#04d361" />
+            Luana Campos
+            <FiX color="#737380" className={styles.closeIcon} />
+        </button>
+    ) : (
+        <button 
+            type="button" 
+            className={styles.signinButton}
+        >
+            <FaGithub color="#eba417" />
+            Sign in with GitHub
+        </button>
+    )
+}
