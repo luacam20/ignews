a18c2658ee7ec5bf5acf5d5cc819cd9211973ccd
luacam20 <luana.cam20@gmail.com>
2022-01-19 10:21:17 -0300

    Autenticaçao com git

--- src/components/SiginButton/index.tsx
+++ src/components/SiginButton/index.tsx
@@ -1,25 +1,31 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, useSession, signOut } from 'next-auth/client'
 
 import styles from './styles.module.scss'
 
 export function SigInButton() {
     
-    const isUserLoggedIn = true;
-
-    return isUserLoggedIn ? (
+    //if o usuario esta logado 
+    const [session] = useSession()
+    
+    console.log(session)//informações do login
+    
+    return session ? (
         <button 
             type="button" 
             className={styles.signinButton}
+            onClick={()=> signOut()}
         >
             <FaGithub color="#04d361" />
-            Luana Campos
+            {session.user.name}
             <FiX color="#737380" className={styles.closeIcon} />
         </button>
     ) : (
         <button 
             type="button" 
             className={styles.signinButton}
+            onClick={() => signIn('github')}
         >
             <FaGithub color="#eba417" />
             Sign in with GitHub
