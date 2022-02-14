e3fd4d5c75e9eb6e19e3ecdd58c4996eab5d9c71
luacam20 <luana.cam20@gmail.com>
2022-02-14 15:34:15 -0300

    Pagina de posts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -18,6 +18,42 @@ export default NextAuth({
   //   signInKey: process.env.SIGING_KEY,
   // },
   callbacks: {
+    async session (session) {
+      try{
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  "ref", 
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(session.user.email)
+              )
+            ])
+          )
+        ) 
+  
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription
+        }
+      } catch (e) {
+        return {
+          ...session,
+          activeSubscription: null
+        }
+      }
+    },
     async signIn( user, account, profile) {
       
       const { email } = user;
